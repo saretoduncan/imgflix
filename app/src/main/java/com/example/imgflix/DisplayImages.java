4316c2cc03647ec9cad2b37e678dd05c90562e83
saretoduncan <saretoduncan@gmail.com>
2021-09-06 07:06:26 +0300

    complete signup page

---
+++ app/src/main/java/com/example/imgflix/DisplayImages.java
@@ -0,0 +1,14 @@
+package com.example.imgflix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayImages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_images);
+    }
+}
